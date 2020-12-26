a8cc5d573b1a0fb961b03a52a16babeffe8e77ce
apap26 <apap26@yandex.ru>
2020-12-27 04:55:10 +0500

    Добавлена верска

--- GAI/views.py
+++ GAI/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+
+@login_required
+def index(request):
+    return render(request, "home.html", {})
+
+def create_dtp(request):
+    if (request.method == "GET"):
+        return render(request, "addCard.html", {})
+    elif request.method == "POST":
+        card_number = request.POST.get("card_number")
+        dateTime = request.POST.get("date")
+        place = request.POST.get("place")
+        type_dtp = request.POST.get("type")
+
 
 # Create your views here.
